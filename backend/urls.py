5aa937ce849d274bff25f63d7259baad1f9173fe
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-10-23 21:06:30 +0300

    refactore project structure

--- backend/urls.py
+++ backend/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework import routers
-from blog import views
+
+from blog.api import views
+
 router = routers.DefaultRouter()
-router.register(r'blogs', views.BlogView, 'blog')
+router.register("blogs", views.BlogView, "blog")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
